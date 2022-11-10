5da1bdc66482c6586805333f159d1b3d2a543bab
Rituparna Addy <93533786+Ritu000addy@users.noreply.github.com>
2022-11-11 00:09:15 +0530

    update

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -59,7 +59,7 @@ class DataIngestion:
     def initiate_data_ingestion(self) -> DataIngestionArtifact:
         try:
             dataframe = self.export_data_into_feature_store()
-            self.split_data_as_train_test(dataframe= dataframe)
+            self.split_data_as_train_test(dataframe=dataframe)
             data_ingestion_artifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path, 
             test_file_path=self.data_ingestion_config.testing_file_path)
             return data_ingestion_artifact
