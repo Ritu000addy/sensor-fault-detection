0d617a3537e04a72a4643f604503fe8c6888ec25
Rituparna Addy <93533786+Ritu000addy@users.noreply.github.com>
2022-11-11 05:02:07 +0530

    update

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,8 +1,9 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
 from sensor.constant.env_variable import MONGODB_URL_KEY
+from sensor.exception import SensorException
 import certifi
-import os
+import os, sys
 ca = certifi.where()
 class MongoDBClient:
     client = None
@@ -10,6 +11,7 @@ class MongoDBClient:
         try:
             if MongoDBClient.client is None:
                 #mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                #mongo_db_url="mongodb+srv://avnish:XglZZ9OkjjUw74pZ@ineuron-ai-projects.7eh1w4s.mongodb.net/admin?authSource=admin&replicaSet=atlas-okvkrd-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
                 mongo_db_url = "mongodb+srv://Ritu_addy:ritumongo123@cluster0.wkjig8i.mongodb.net/?retryWrites=true&w=majority"
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile = ca)
 
@@ -17,4 +19,4 @@ class MongoDBClient:
             self.database = self.client[database_name]
             self.database_name = database_name
         except Exception as e:
-            raise e
+            raise SensorException(e, sys)
