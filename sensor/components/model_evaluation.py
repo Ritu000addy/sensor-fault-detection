f61ec7e7db9a2e363e1bf5e567ec398622ff242f
Rituparna Addy <93533786+Ritu000addy@users.noreply.github.com>
2023-04-12 16:51:06 +0530

    update

--- sensor/components/model_evaluation.py
+++ sensor/components/model_evaluation.py
@@ -51,6 +51,7 @@ class ModelEvaluation:
                     best_model_metric_artifact= None)
 
                 logging.info(f"Model evaluation artifact: {model_evaluation_artifact}")
+                write_yaml_file(self.model_eval_config.report_file_path, model_evaluation_artifact.__dict__)
                 return model_evaluation_artifact
 
             latest_model_path = model_resolver.get_best_model_path()
